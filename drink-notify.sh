d842036b2b819e5adf49fb69edfe56567b12dbf4
mozhu <mozhu@in66.com>
2016-06-29 18:21:20 +0800

    add log

--- drink-notify.sh
+++ drink-notify.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-DISPLAY=:0.0
-XAUTHORITY=/home/mozhu/.Xauthority
-
 if [ -r "$HOME/.dbus/Xdbus"  ]; then
       . "$HOME/.dbus/Xdbus"
 fi
 
-
-
-
-
-
-
-
-
 /usr/bin/notify-send "定时喝水啦！" -i "/home/mozhu/scripts/drink/water.jpg"
+echo "ding" >> /tmp/drink.log
+date >> /tmp/drink.log
